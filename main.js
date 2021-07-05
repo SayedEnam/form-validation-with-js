5cf726bfd9d79e6f1be9b3969a8ff425aa7ec251
SayedEnam <syedenamshohag@gmail.com>
2021-07-05 14:29:44 +0600

    Simple Form with validation

---
+++ main.js
@@ -0,0 +1,21 @@
+const name = document.getElementById('name');
+const mail = document.getElementById('mail');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e)=> {
+  let messages = [];
+  if(name.value === '' || name.value == null){
+    messages.push('Name is required');
+  }
+  if(password.value.length <= 6){
+    messages.push ('Password must be longer than 6 character');
+  }
+
+  if(messages.length > 0){
+    e.preventDefault();
+    errorElement.innerText = messages.join(', ');
+  }
+  
+})
